11ee2b6544cb66a74525596717c1f88a38a5c4c2
Oluwasayo Babalola <sayob02@gmail.com>
2017-03-30 17:18:24 +0100

    Create arraysum.php

---
+++ arraysum.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Sum of Array</title>
+</head>
+
+<body>
+
+<?php
+
+$my_array = array(6,10,15,70,40,4,5,50);
+echo array_sum($my_array);      //prints 200
+?>
+
+</body>
+
+
+</html>
